05080290e6a32fcae137c716a10b045d171b2b65
jmrego77 <60928863+jmrego77@users.noreply.github.com>
2022-01-27 18:16:57 -0500

    Update boilerplate-theme.js

--- src/components/UXPinWrapper/boilerplate-theme.js
+++ src/components/UXPinWrapper/boilerplate-theme.js
@@ -4,13 +4,13 @@ const boilerplateTheme = {
   palette: {
     mode: "light",
     primary: {
-      main: "#1976d2",
+      main: "#93186c",
       light: "#42a5f5",
       dark: "#1565c0",
       contrastText: "#ffffff"
     },
     secondary: {
-      main: "#9c27b0",
+      main: "#16666f",
       light: "#ba68c8",
       dark: "#7b1fa2",
       contrastText: "#ffffff"
